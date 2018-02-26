4d275056588d4a121aee822d5adcf2f980d45691
Alex Lic Chong Voong <15082895@stu.mmu.ac.uk>
2018-02-26 14:56:03 +0000

    timer

--- TwinShooterProject/Assets/Scripts/ElapsedTimeScript.cs
+++ TwinShooterProject/Assets/Scripts/ElapsedTimeScript.cs
@@ -1,25 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ElapsedTimeScript : MonoBehaviour
 {
 
 	public static float timer;
-	
+	public Text timerText;
+	public float speed = 1;
+
 	// Update is called once per frame
 	void Update () 
 	{
-		timer += Time.deltaTime;
-	}
+		timer += Time.deltaTime * speed;
 
-	void OnGUI()
-	{
-		int minutes = Mathf.FloorToInt (timer / 60F);
-		int seconds = Mathf.FloorToInt (timer - minutes * 60);
-		string elapsedTime = string.Format ("{0:0}:{1:00}", minutes, seconds);
+		string hours = Mathf.Floor ((timer % 21600) / 3600).ToString ("00");
+		string minutes = Mathf.Floor((timer % 3600) / 60).ToString ("00");
+		string seconds = (timer % 60).ToString ("00");
 
-		GUI.Label (new Rect (10, 10, 250, 100), elapsedTime);
+		timerText.text = hours + ":" + minutes + ":" + seconds;
+
 
 	}
 }
