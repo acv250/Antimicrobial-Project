4591215c7e4c044476cdfdd162b117c9e1920f76
Alex Lic Chong Voong <15082895@stu.mmu.ac.uk>
2018-04-21 14:11:46 +0100

    blood stream

---
+++ TwinShooterProject/Assets/Scripts/RedBloodCellScripts/RedBloodCellScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RedBloodCellScript : MonoBehaviour 
+{
+
+	public float moveSpeed;
+	public float rotationSpeed;
+	private float bloodVelocity;
+
+	public GameObject bloodSprite;
+
+	// Use this for initialization
+	void Start () 
+	{
+		moveSpeed = Random.Range (2.0f, 6.0f);
+		rotationSpeed = Random.Range (-10.0f, 1.0f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate (Vector3.left * Time.deltaTime * moveSpeed, Space.World);
+		transform.Rotate (0, 0, rotationSpeed);
+
+		if (transform.position.x <= -10f) 
+		{
+			Destroy (bloodSprite);
+		}
+	}
+
+}
---
+++ TwinShooterProject/Assets/Scripts/RedBloodCellScripts/RedBloodSpawnManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedBloodSpawnManager : MonoBehaviour 
+{
+
+	public GameObject[] bloodSprite;
+	public Vector2 spawnValues;
+	public float spawnWait;
+	public float spawnMinWait;
+	public float spawnMaxWait;
+	public int startWait;
+	public bool stopSpawn;
+
+	private int bloodID;
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine (SpawnBlood ());
+		stopSpawn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		bloodID = 0;
+		spawnWait = Random.Range (spawnMinWait, spawnMaxWait);
+	}
+
+	IEnumerator SpawnBlood()
+	{
+		yield return new WaitForSeconds (startWait);
+
+		while(!stopSpawn)
+		{
+			
+			Vector2 spawnPosition = new Vector2 (spawnValues.x, Random.Range (-spawnValues.y, spawnValues.y));
+
+			Instantiate (bloodSprite [bloodID], spawnPosition, gameObject.transform.rotation);
+
+			yield return new WaitForSeconds (spawnWait);
+		}
+	}
+}
